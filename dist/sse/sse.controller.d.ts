8531705a6fc8a957a6b45457da665fee52c158ae
Carlos Filho <d3_nt@hotmail.com>
2024-06-10 17:57:01 +0000

    http1

--- dist/sse/sse.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { Observable } from 'rxjs';
-export declare class SseController implements OnModuleInit {
-    private eventSubject;
-    private ws;
-    onModuleInit(): void;
-    private connectWebSocket;
-    sendEvents(): Observable<MessageEvent>;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as https from 'https';
-
-const express = require('express');
 
 async function bootstrap() {
-  // Configuração do Express para NestJS
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
 
   // Habilitar CORS (ajuste conforme necessário)
   app.enableCors({
@@ -18,21 +11,7 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept',
   });
 
-  // Carregar os certificados SSL/TLS
-  const httpsOptions = {
-    key: fs.readFileSync('/home/carloscoelho/projects/ouribank/sse-api/server.key'),
-    cert: fs.readFileSync('/home/carloscoelho/projects/ouribank/sse-api/server.cert'),
-  };
-
-  // Criar servidor HTTP/2 usando o módulo https com HTTP/2 habilitado
-  const httpsServer = https.createServer(httpsOptions, server);
-
-  await app.init();
-  
-  // Iniciar o servidor HTTP/2
-  httpsServer.listen(3000, () => {
-    console.log('NestJS server running with HTTP/2 on https://localhost:3000');
-  });
+  await app.listen(3000);
 }
 
 bootstrap();
